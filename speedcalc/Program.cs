c46c9eb1f844f3a68b486ebb01f8b357e7601dd3
BabakAdam <babaka@kkszki.hu>
2024-10-11 12:05:12 +0200

    Add project files.

---
+++ speedcalc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedcalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed;
+
+            
+            do
+            {
+              Console.Write("Kérlek, add meg a futó kezdő sebességét (3.00 - 5.00 m/s): ");
+              speed = double.Parse(Console.ReadLine());
+            } while (speed < 3.00 || speed > 5.00);
+
+            Console.WriteLine("Sebesség tíz méterenként:");
+
+            
+            for (int i = 0; i <= 100; i += 10)
+            {
+                double currentSpeed;
+
+                if (i < 90)
+                {
+                    currentSpeed = speed + (2 * (i / 90.0));
+                }
+                else
+                {
+                    
+                    currentSpeed = speed + 2 - (2 * ((i - 90) / 10.0)); 
+                }
+
+                
+                double speedInKmh = currentSpeed * 3.6;
+                Console.WriteLine($"{i} méternél: {speedInKmh:F2} km/h");
+            }
+        }
+    }
+}
